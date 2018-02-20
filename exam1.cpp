c7679258c6a0b0ec695c75919d9850ac22909c5b
Unknown <lovechillyhot@hotmail.com>
2018-02-20 12:59:00 +0700

    เสร็จแล้วค่ะ

--- exam1.cpp
+++ exam1.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
-
-int main() {
-
-        return 0;
+using namespace std;
+int main () {
+    int a,b,cmd,reusult_1,reusult_2,reusult_3,reusult_4;
+    cout<<"Welcome to CLL Calculator V1.00"<<endl;
+    cout<<"Number 1 : ";
+    cin>>a;
+    cout<<"Number 2 : ";
+    cin>>b;
+    do{
+        cout<<"menu\n1.+\n2.-\n3.*\n4./\n5.quit"<<endl;
+        cin>>cmd;
+    if(cmd==1){
+        reusult_1=a+b;
+        cout<<"Resulf = "<<reusult_1<<endl;
+    }else if (cmd==2){
+        reusult_2=a-b;
+        cout<<"Resulf = "<<reusult_2<<endl;
+    }else if (cmd==3){
+         reusult_3=a*b;
+        cout<<"Resulf = "<<reusult_3<<endl;
+    }else if (cmd==4){
+        reusult_4=a/b;
+        cout<<"Resulf = "<<reusult_4<<endl;
+    }{break;}
+    }while(1);
+    return 0;
 }
+
+
